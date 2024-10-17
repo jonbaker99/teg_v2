fa6e65c07c51ea094ade1b95ab3151ed594f2250
jonbaker99 <jonbaker99@hotmail.com>
2024-10-17 18:17:58 +0100

    ave by course

--- streamlit/ave_by_course.py
+++ streamlit/ave_by_course.py
@@ -8,73 +8,17 @@ datawrapper_table_css()
 st.title('Average score by Course')
 
 all_data = load_all_data(exclude_incomplete_tegs=False)
-# avg_grossvp = all_data.groupby(['Player', 'Course'])['GrossVP'].mean().unstack(fill_value=0)
-# avg_grossvp['Total'] = all_data.groupby('Player')['GrossVP'].mean()
-# avg_grossvp = avg_grossvp.sort_values('Total', ascending=True)
-# avg_grossvp = avg_grossvp.round(2)
 
-
-# def format_value(value):
-#     if value > 0:
-#         return f"+{value:.2f}"
-#     elif value < 0:
-#         return f"{value:.2f}"
-#     else:
-#         return "="
-
-# for column in avg_grossvp.columns:
-#     avg_grossvp[column] = avg_grossvp[column].apply(format_value)
-
-# avg_grossvp.reset_index(inplace=True)
-# avg_grossvp.columns.name = None
-# avg_grossvp.columns = ['Player'] + [f'Par {col}' if col != 'Total' else col for col in avg_grossvp.columns[1:]]
-
-# st.write(avg_grossvp.to_html(classes='dataframe, datawrapper-table', index=False, justify='left'), unsafe_allow_html=True)
-
-# avg_grossvp = all_data.groupby(['Player', 'Course'])['GrossVP'].mean().unstack(fill_value=0)
-# avg_grossvp['Total'] = all_data.groupby('Course')['GrossVP'].mean()
-# avg_grossvp = avg_grossvp.sort_values('Total', ascending=True)
-# avg_grossvp = avg_grossvp.round(1)
-
-
-# def format_value(value):
-#     if value > 0:
-#         return f"+{value:.2f}"
-#     elif value < 0:
-#         return f"{value:.2f}"
-#     else:
-#         return "-"
-
-# for column in avg_grossvp.columns:
-#     avg_grossvp[column] = avg_grossvp[column].apply(format_value)
-
-# avg_grossvp.reset_index(inplace=True)
-# avg_grossvp.columns.name = None
-# avg_grossvp.columns = ['Course'] + [f'{col}' if col != 'Total' else col for col in avg_grossvp.columns[1:]]
-
-# st.write(avg_grossvp.to_html(classes='dataframe, datawrapper-table', index=False, justify='left'), unsafe_allow_html=True)
-
-
-# # Step 1: Group by Course and Player to calculate the mean GrossVP for each player in each course
-# summary = all_data.groupby(['Course', 'Pl'])['GrossVP'].mean().unstack(fill_value=0)
-
-# # Step 2: Add a 'Total' row for each course, calculating the average GrossVP across all players
-# summary['Total'] = summary.mean(axis=1)
-
-# # Step 3: Transpose the DataFrame so that Courses are rows and Players are columns
-# summary_transposed = summary.T
-
-# # Step 4: Add a 'Total' column for each player (mean across all courses)
-# summary_transposed['Total'] = summary_transposed.mean(axis=1)
-
-# # Step 5: Convert the transposed DataFrame to an HTML table
-# html_table = summary_transposed.to_html(classes='dataframe, datawrapper-table', index=True, justify='left')
-
-# # Step 6: Display the table in Streamlit
-# st.write(html_table, unsafe_allow_html=True)
-
-
-'---'
+st.markdown(
+    """
+    <style>
+    [data-testid="stElementToolbar"] {
+        display: none;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
 
 
 # Calculate the mean of GrossVP for each player in each course
