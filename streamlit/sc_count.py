53d40465e14a4774fdcf33ec9675410784be8adf
jonbaker99 <jonbaker99@hotmail.com>
2024-10-21 17:58:31 +0100

    Update sc_count.py

---
+++ streamlit/sc_count.py
@@ -0,0 +1,34 @@
+import streamlit as st
+import pandas as pd
+from utils import load_all_data, datawrapper_table_css
+
+all_data = load_all_data(exclude_incomplete_tegs = False)
+datawrapper_table_css()
+
+st.title('Count of score by player')
+
+all_data = load_all_data(exclude_incomplete_tegs = True)
+
+def count_by_pl(df = all_data, field = 'GrossVP')
+
+    summary = all_data.groupby(['GrossVP', 'Pl']).size().unstack(fill_value=0)
+    
+    # Sort the index (GrossVP) in descending order
+    summary = summary.sort_index(ascending=True)
+    
+    # Optional: Sort columns (players) alphabetically
+    summary = summary.sort_index(axis=1)
+    
+    return summary
+    
+
+count_gvp = count_by_pl(all_data, 'GrossVP')
+count_sc = count_by_pl(all_data, 'Sc')
+
+st.markdown('### Count of Gross vs Par by player')
+st.write(count_gvp)
+
+'---'
+
+st.markdown('### Count of gross score by player')
+st.write(count_sc)
