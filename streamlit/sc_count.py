ba8bcdd56b32e2ab562f5b3e2afbaadc1a5a9b54
jonbaker99 <jonbaker99@hotmail.com>
2024-10-25 09:36:33 +0100

    update to score count page

--- streamlit/sc_count.py
+++ streamlit/sc_count.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-from utils import load_all_data, datawrapper_table_css
+from utils import load_all_data, datawrapper_table_css, datawrapper_table, format_vs_par
 
 all_data = load_all_data(exclude_incomplete_tegs = False)
 datawrapper_table_css()
@@ -36,12 +36,23 @@ def count_by_pl(df = all_data, field = 'GrossVP'):
 count_gvp = count_by_pl(filtered_data, 'GrossVP')
 count_sc = count_by_pl(filtered_data, 'Sc')
 
-st.markdown('### Count of Gross vs Par by player')
-st.dataframe(count_gvp, height = len(count_gvp) * 35 + 38)
-#st.write(count_gvp.to_html(classes='datawrapper-table'), unsafe_allow_html=True)
-'---'
 
-st.markdown('### Count of gross score by player')
-st.dataframe(count_sc, height = len(count_sc) * 35 + 38)
-#st.write(count_sc)
-#st.write(count_sc.to_html(classes='datawrapper-table'), unsafe_allow_html=True)
+tab1, tab2  = st.tabs(["Scores", "Scores vs Par"])
+
+with tab1:
+    st.markdown('### Count of Gross vs Par by player')
+    # st.dataframe(count_gvp, height = len(count_gvp) * 35 + 38)
+    count_gvp = count_gvp.reset_index()
+    count_gvp['GrossVP'] = count_gvp['GrossVP'].apply(format_vs_par)
+    count_gvp = count_gvp.rename(columns={'GrossVP': 'vs Par'})
+    count_gvp.columns.name = None
+    datawrapper_table(count_gvp)
+
+with tab2:
+    st.markdown('### Count of gross score by player')
+    # st.dataframe(count_sc, height = len(count_sc) * 35 + 38)
+    count_sc = count_sc.reset_index()
+    count_sc.columns.name = None
+    count_sc['Sc'] = count_sc['Sc'].astype(int)
+    count_gvp = count_gvp.rename(columns={'Sc': 'Score'})
+    datawrapper_table(count_sc)
